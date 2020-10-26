f9aeff7cb8bb40786a8ce184b2ce6928516584c9
The Creative DEV <thecreativedev@outlook.fr>
2020-10-26 14:28:31 +0100

    Adding Comments in file 'index.php'

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
     <title>Formulaire de Selection</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
+        /* Reset all Styles */
         * {
             box-sizing: border-box;
             padding: 0;
             margin: 0;
             font-family: 'Indie Flower', cursive;
         }
+        /* Stylizing Globale */
         body {
             min-height: 100vh;
             display: flex;
@@ -21,16 +23,19 @@
             background-color: #0377fc;
             font-size: 36px;
         }
+        /* Selecteur multiple */
         select, input {
             font-size: 24px;
             padding: 9px 18px;
         }
+        /* Selecteur Titre */
         h1 {
             font-size: 50px;
         }
         form {
             margin: calc(100vh - 80vh) 0;
         }
+        /* Classes */
         .respond {
             background-color: green;
             padding: 10px 25px;
@@ -46,31 +51,38 @@
 </head>
 <body>
     <h1>Exercice PHP - Veuillez choisir une ville :</h1>
-
+    <!-- Ajout du formulaire avec la methode post pour eviter d'afficher le lien dans l'url -->
     <form method="post">
-        <label for="ville">Sélectionner une ville :</label>
-        <select name="ville" id="ville">
+        <label for="villeLabel">Sélectionner une ville :</label>
+        <!-- ID Pour le label; Name: Permettant de récupérer la data dans le tableau Post -->
+        <select name="ville" id="villeLabel">
+            <!-- Juste un groupe pour biens que ce sont des noms de villes .. -->
             <optgroup label="Villes :">
-                <option selected disabled hidden value=" ">Choissiez une ville !</option>
+                <!-- Juste option de sélection invisible et autres parametre -->
+                <option selected disabled hidden>Choissiez une ville !</option>
                 <option value="Paris">Paris</option>
                 <option value="Berlin">Berlin</option>
                 <option value="Mouscou">Mouscou</option>
             </optgroup>
         </select>
+        <!-- Bouton d'envoi de Formulaire -->
         <input type="submit" value="Envoyer la réponse">
     </form>
 
     <?php
+        // Stockage de variables pour des contenus récurrentes
         $startPara = '<p class="respond">';
         $endPara = '</p>';
         $startPhrase = 'La ville de ';
         $endPhrase = ' est la capitale de la ';
-
+        // Condition SI : La variable POST est vide alors fait cela SINON ...
         if (empty($_POST)) {
             echo ('<p class="respond error">' . "Veuillez faire un choix !" . $endPara);
         }
         else {
+            // Récuperation et stockage de la variable Super-Globale 
             $ville = $_POST["ville"];
+            // Conditions pour chaques cas faire ...
             switch ($ville) {
                 case 'Paris':
                     echo($startPara . $startPhrase . $ville . $endPhrase . 'France' . $endPara);
@@ -81,8 +93,9 @@
                 case 'Mouscou':
                     echo($startPara . $startPhrase . $ville . $endPhrase . 'Russie' . $endPara);
                     break;
+                // SI aucun des cas de ces derniers énumérées alors afficher  
                 default:
-                    echo("Erreur inconnue !");
+                    echo('<p class="respond error">'."Erreur inconnue !".$endPara);
                     break;
             }
         }
