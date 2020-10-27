6e05d5cd30704cf236833e197ee04fe963a2856c
The Creative DEV <thecreativedev@outlook.fr>
2020-10-27 16:14:31 +0100

    Missed Commit last modification

---
+++ Test_debug.php
@@ -0,0 +1,15 @@
+<?php
+    $tableau = ["Test"];
+    $number = 1;
+    $string = "Test";
+
+    //Retourne le type de la variable ainsi que le nombre de carattère
+    echo("Resultat du var dump : ".var_dump($string)." <br>");
+    echo("<br>");
+    // Retourne le contenu de la variable
+    echo("Resultat du print_r : ".print_r($string)." <br>");
+    echo("<br>");
+    //
+    echo("Resultat du printf : ".printf($string)." <br>");
+    echo("<br>");
+?>
